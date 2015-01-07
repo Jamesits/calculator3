10cc3377eda262db6cb0ae93b34a9eb41ed20d43
James Swineson <jamesswineson@gmail.com>
2015-01-07 15:44:29 +0800

    Initial Commit

---
+++ calculator3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  calculator3
+//
+//  Created by James Swineson on 15/1/7.
+//  Copyright (c) 2015年 James Swineson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
